590c43421bdb78b541912ef4c47c5b0f58f5548d
John Cervantes <crzylnr@gmail.com>
2015-09-02 10:05:34 -0700

    User input still not worked on ... finding large num is done .. working on print function :D

--- chart/chart/chart.cpp
+++ chart/chart/chart.cpp
@@ -10,15 +10,15 @@ int largestNumber(int valuesCharge[]);
 
 int main()
 {
-	const int MAX = 100;
-	int values[MAX];
+	const int MAX = 5;
+	int values[MAX] = {51, 2, 3, 40, 5};
 	int large;
 	int i = -1;
 
-	do {
+	/*do {
 		i++;
 		cin >> values[i];
-	} while (values[i] != 0);
+	} while (values[i] != 0);*/
 
 	large = largestNumber(values);
 	printChart(values, large);
@@ -26,11 +26,26 @@ int main()
     return 0;
 }
 
-int printChart(int valuesChart[], int largeChart) {
-	int printI = -1;
+int printChart(int valuesChart[], int largeNum) {
+	/*int printI = -1;
 	do {
 		cout << printI << "\n" << valuesChart[printI] << "\n" ;
-	} while (printI < sizeof(valuesChart)/sizeof(int));
+	} while (printI < sizeof(valuesChart)/sizeof(int));*/
+
+
+
+	for (int y = 0; y <largeNum; y++) {
+		for (int x = 0;  x < 4; x++) {
+			if (valuesChart[x] >= 4 - y) {
+				//system("pause");
+				cout << "x" << endl;
+			}
+			else
+				cout << " ";
+		}
+	}
+	cout << endl;
+
 
 	system("pause");
 	///nice xD
@@ -42,6 +57,22 @@ int largestNumber(int valuesLarge[])
 {
 	cout << "LARGE \n";
 	int largeValue = 0;
+	int x = 0;
+	int y = -1;
+
+	do {
+		x++;
+		y++;
+
+		if (valuesLarge[y] < valuesLarge[x] && valuesLarge[x] > largeValue) {
+			largeValue = valuesLarge[x];
+		}
+		if (valuesLarge[y] > valuesLarge[x] && valuesLarge[y] > largeValue) {
+			largeValue = valuesLarge[y];
+		}
+	} while (y < 10);
+
+	cout << "Largest number is " << largeValue << endl;
 	return largeValue;
 }
 
